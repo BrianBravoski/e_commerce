913fcb78aaac518a41e5979f79ac3d0f9a183b8d
BrianBravoski <brianbravoski28@gmail.com>
2022-06-11 23:13:36 +0300

    components page and herobanner component

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -2,19 +2,26 @@ import Head from 'next/head'
 import Image from 'next/image'
 import product from '../sanity_ecommerce/schemas/product'
 
+import React from 'react';
+
+import { Product, FooterBanner, HeroBanner } from '../components'
+
 export default function Home() {
   return (
   <>
-    HeroBanner
-      <div>
+  <  HeroBanner />
+      <div className='products-heading'>
       <h2>Best Selling Products</h2>
       <p>Speakers Variations</p>
       </div>
 
-      <div>
+      <div className='products-container'>
         {['Product 1','Product 2'].map(
           (product) => product)}
       </div>
+
+      <FooterBanner/>
+      
   </>
 
   )
