8d47f30ffa2fa23f24dca9562e210635af7f45db
BrianBravoski <brianbravoski28@gmail.com>
2022-06-13 23:28:24 +0300

    sanity connection (i)

--- ecommerce/lib/client.js
+++ ecommerce/lib/client.js
@@ -7,4 +7,9 @@ export const client = sanityClient({
     apiVersion:'2022-03-10',
     useCdn:'true',
     token:process.env.NEXT_PUBLIC_SANITY_TOKEN
-})
+});
+
+const builder = imageUrlBuilder(client);
+
+export const urlFor =(source) => builder.image(source);
+
--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -2,26 +2,40 @@ import Head from 'next/head'
 import Image from 'next/image'
 import product from '../sanity_ecommerce/schemas/product'
 
+import {client} from '../lib/client'
+
 import React from 'react';
 
 import { Product, FooterBanner, HeroBanner } from '../components'
 
-export default function Home() {
+export const getServerSideProps= async () =>{
+  const query = '*[_type== "product"]';
+  const products = await client.fetch (query);
+
+  const bannerQuery = '*[_type== "banner"]';
+  const bannerData = await client.fetch (bannerQuery);
+
+  return{
+    props:{ products, bannerData}
+  }
+}
+
+export default function Home( products, bannerData) {
   return (
   <>
-  <  HeroBanner />
+  <  HeroBanner heroBanner={} />
       <div className='products-heading'>
       <h2>Best Selling Products</h2>
       <p>Speakers Variations</p>
       </div>
 
       <div className='products-container'>
-        {['Product 1','Product 2'].map(
-          (product) => product)}
+        {products?.map(
+          (product) => product.name)}
       </div>
 
       <FooterBanner/>
-      
+
   </>
 
   )
