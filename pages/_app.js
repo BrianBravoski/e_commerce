9612f25ed67eb666a995d0fcd666c5cd2f7f5ff5
BrianBravoski <brianbravoski28@gmail.com>
2022-10-24 13:39:20 +0300

    quantity number is dynamic with state context

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import '../styles/globals.css'
+import { Toaster } from 'react-hot-toast';
 
-
-import {Layout} from '../components'
+import { StateContext } from '../context/StateContext';
+import { Layout } from '../components';
 
 
 function MyApp({ Component, pageProps }) {
 
-  return( 
-  <Layout>
-      <Component {...pageProps} />
-  </Layout>
+  return (
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
 
   )
 }
